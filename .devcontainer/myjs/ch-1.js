8ba864aa9b428d5a4b0b9f662ace261221bc68c1
Durgesh pandey <cybersarthi245@gmail.com>
2026-05-14 08:29:09 +0000

    new-style

---
+++ .devcontainer/myjs/ch-1.js
@@ -0,0 +1,9 @@
+console.log ("HELLO MR.Hacker")
+//alert("Hac"ked devide")
+let string ="durgesh"
+ let number= 123
+ let email= "harrypotter"
+let c =1038
+ const as="delhi"
+ console.table([string, number, email]);
+ 
